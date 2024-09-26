c864715b82d70e711fbd59545e408539918382e6
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-09-26 14:25:59 +0200

    Add files via upload

---
+++ KURUS.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+void main(){
+  print("Kérem az egész számot");
+  int? beker = int.parse(stdin.readLineSync()!);
+  try {
+    int number = int.parse(beker!);
+    if (number <= 0) {
+      throw FormatException('Érvénytelen érték');
+    }
+    int modulo = number % 3;
+    if (modulo == 0) {
+      print('Nullával nem lehet elosztani!');
+    } else {
+      int result = number ~/ modulo;
+      print('Az eredmény értéke: $result');
+    }
+  } on FormatException catch(e) {
+    print(e.message);
+    print('A megadott szám: ${beker}');
+  } catch (e) {
+    print('Ismeretlen hiba: $e');
+  } finally {
+    print('Finally block always executed');
+  }
+}
