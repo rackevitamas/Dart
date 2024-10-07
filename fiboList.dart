432fad02f81b48f616190557ae86dbab5360585b
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-10-07 09:46:55 +0200

    Add files via upload

--- fiboList.dart
+++ fiboList.dart
@@ -1,9 +1,9 @@
 void main(){
-  List<int> fiboList = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (int i = 2; i < fiboList.length; i++){
-    fiboList[2] = fiboList[0] + fiboList[1];
-    fiboList[0] = fiboList[1];
-    fiboList[1] = fiboList[2];
-    print('$fiboList');
+  List<int> fiboList = [1, 1];
+  while (true) {
+    int nextFibo = fiboList[fiboList.length - 1] + fiboList[fiboList.length - 2];
+    if (nextFibo >= 30) break;
+    fiboList.add(nextFibo);
   }
+  print('fiboList: $fiboList');
 }
