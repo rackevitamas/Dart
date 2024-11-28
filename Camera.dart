776b247e06f5b0fd2ed509a9cde06048aae388b0
Rác Kevi Tamás <90187820+rackevitamas@users.noreply.github.com>
2024-11-28 16:18:14 +0100

    Update Camera.dart

--- Camera.dart
+++ Camera.dart
@@ -6,16 +6,15 @@ class Camera{
 
      Camera(this.brand, this.resulotion, this.canVideo)
 
-     void Kiir(){
-            print("A fényképezőgép márkája: $ {cam.brand}, felbontása: ${cam.resulotion},  képes felvenni a videót: ${cam.canVideo}.");
-}
+void Kiir(){
+    print("A fényképezőgép márkája: ${cam.brand}, felbontása: ${cam.resulotion}, képes felvenni a videót: ${cam.canVideo}.");
 }
 
 
 
 void main(){
-      Camera cam = Camera("Sony", "4K ultra       HD", true);
-      Camera cam2 = Camera("Sony", "4K     ultra HD", true);
+      Camera cam = Camera("Sony", "4K ultra HD", true);
+      Camera cam2 = Camera("Nikon", "4K     ultra HD", false);
       cam.Kiir();
       cam2.Kiir();
 }
