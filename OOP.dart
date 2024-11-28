e5a27229a6a9b402b4e9b058d6447b20208ec581
Rác Kevi Tamás <90187820+rackevitamas@users.noreply.github.com>
2024-11-28 15:52:03 +0100

    Update OOP.dart

--- OOP.dart
+++ OOP.dart
@@ -1,11 +1,12 @@
 class Laptop {
   String? brand;
   int? price;
+  bool? gameLaptop;
 
-  Laptop(this.brand, this.price);
+  Laptop(this.brand, this.price, this.gameLaptop);
 }
 
 void main() {
-  Laptop laptop = Laptop("MSI", 490000);
-  print("A laptop márkája: ${laptop.brand}, ára: ${laptop.price} Ft.");
+  Laptop laptop = Laptop("MSI", 490000, true);
+  print("A laptop márkája: ${laptop.brand}, ára: ${laptop.price} Ft, dedikált VGA: ${laptop.gameLaptop}.");
 }
