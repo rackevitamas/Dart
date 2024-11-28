2fcfa66afc8d08f4567c829a7a201a8e838e2947
Rác Kevi Tamás <90187820+rackevitamas@users.noreply.github.com>
2024-11-28 15:48:09 +0100

    Update OOP.dart

--- OOP.dart
+++ OOP.dart
@@ -1,15 +1,11 @@
-class Laptop{
+class Laptop {
   String? brand;
   int? price;
 
-  //Constructor
-  //Laptop(this.brand, this.price);
-   
+  Laptop(this.brand, this.price);
 }
 
-void main(){
-  Laptop laptop = new Laptop();
-  laptop.brand = "MSI";
-  laptop.price = 490000;
+void main() {
+  Laptop laptop = Laptop("MSI", 490000);
   print("A laptop márkája: ${laptop.brand}, ára: ${laptop.price} Ft.");
 }
