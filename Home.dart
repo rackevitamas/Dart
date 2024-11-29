a1da7e68c0bc1db9649a94fca222bf680adb8d16
Rác Kevi Tamás <90187820+rackevitamas@users.noreply.github.com>
2024-11-29 17:56:07 +0100

    Update Home.dart

--- Home.dart
+++ Home.dart
@@ -1,20 +1,19 @@
-class Home{
-     String? fajta;
-     String? futesTipus;
-     bool? friendOfNatur;
+class Home {
+  String? fajta;
+  String? futesTipus;
+  bool? friendOfNatur;
 
+  Home(this.fajta, this.futesTipus, this.friendOfNatur);
 
-     Home(this.fajta, this.futesTipus, this.friendOfNatur)
-
-void Kiir(){
-    print("A ház fajtája: ${this.fajta}, fűtés típusa: ${this.futesTipus}, környezetbarát villamosság: ${this.friendOfNatur}.");
+  void kiir() {
+    print(
+        "A ház fajtája: ${this.fajta}, fűtés típusa: ${this.futesTipus}, környezetbarát villamosság: ${this.friendOfNatur}.");
+  }
 }
 
-
-
-void main(){
-      Home hm = Home("Emeletes", "gyár által fűtés", false);
-      Home hm2 = Home("Földszintes", "klíma", true);
-      hm.Kiir();
-      hm2.Kiir();
+void main() {
+  Home hm = Home("Emeletes", "gyár által fűtés", false);
+  Home hm2 = Home("Földszintes", "klíma", true);
+  hm.kiir();
+  hm2.kiir();
 }
